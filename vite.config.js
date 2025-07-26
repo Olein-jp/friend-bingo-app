09514894c90f996032a7727855cc89c4ad206ba6
Koji Kuno <koji.kuno@gmail.com>
2025-07-26 11:35:32 +0900

    Initial deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// GitHub Pages用設定
 export default defineConfig({
-  base: "/friend-bingo-app/",  // ←リポジトリ名に合わせる
+  base: "/friend-bingo-app/", // リポジトリ名に合わせる
   plugins: [react()],
 });
